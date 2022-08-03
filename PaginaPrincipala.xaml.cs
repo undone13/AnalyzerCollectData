c0eaed84b2f7be43a1afcd3c8bdd02d1ebdf3a22
Bogdan Ichim <bogdan_ichim@lovecat.com>
2022-08-03 18:21:53 +0300

    Add project files.

---
+++ PaginaPrincipala.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace AnalyzerCollectData
+{
+    /// <summary>
+    /// Interaction logic for PaginaPrincipala.xaml
+    /// </summary>
+    public partial class PaginaPrincipala : Window
+    {
+        public static string filename = "AnalyzerCollectData.xml";
+
+        public PaginaPrincipala()
+        {
+            InitializeComponent();
+            Loaded += Load;
+        }
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(filename) == false)
+            {
+                CreateXMLFile();
+            }
+        }
+
+        private void buton_Setari_Click(object sender, RoutedEventArgs e)
+        {
+            PaginaSetari paginaSetari = new PaginaSetari();
+            paginaSetari.Show();
+        }
+
+        static void CreateXMLFile()
+        {
+            XmlDocument document = new XmlDocument();
+
+            XmlNode db = document.CreateElement("Database");
+            document.AppendChild(db);
+
+            XmlAttribute type = document.CreateAttribute("type");
+            type.InnerText = "Microsoft SQL Server";
+            db.Attributes.Append(type);
+
+            XmlNode server = document.CreateElement("server");
+            XmlNode dbname = document.CreateElement("dbName");
+            XmlNode username = document.CreateElement("username");
+            XmlNode password = document.CreateElement("password");
+
+            db.AppendChild(server);
+            db.AppendChild(dbname);
+            db.AppendChild(username);
+            db.AppendChild(password);
+
+            document.Save(filename);
+        }
+
+    }
+}
---
+++ PaginaSetari.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace AnalyzerCollectData
+{
+    /// <summary>
+    /// Interaction logic for PaginaSetari.xaml
+    /// </summary>
+    public partial class PaginaSetari : Window
+    {
+        public PaginaSetari()
+        {
+            InitializeComponent();
+            Loaded += Load;
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            CollectXMLData();
+        }
+
+        private void buton_testConexiune_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                string connectionString = "Data Source=" + textBox_DatabaseServer.Text + ";Initial Catalog=" + textBox_DatabaseName.Text + ";Integrated Security=True";
+                SqlConnection sql = new SqlConnection(connectionString);
+                sql.Open();
+
+                MessageBox.Show("Baza de date deschisa cu success!");
+            }
+            catch
+            {
+                MessageBox.Show("Eroare la deschiderea bazei de date!");
+            }
+        }
+
+        private void CollectXMLData()
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(AnalyzerCollectData.PaginaPrincipala.filename);
+            XmlNode root = document.SelectSingleNode("Database");
+
+            XmlAttribute type = document.CreateAttribute("type");
+            root.Attributes.Append(type);
+
+            XmlNode server = root.SelectSingleNode("server");
+            textBox_DatabaseServer.Text = server.InnerText;
+            XmlNode dbname = root.SelectSingleNode("dbName");
+            textBox_DatabaseName.Text = dbname.InnerText;
+            XmlNode username = root.SelectSingleNode("username");
+            textBox_DatabaseUsername.Text = username.InnerText;
+            XmlNode password = root.SelectSingleNode("password");
+            textBox_DatabasePassword.Password = password.InnerText;
+        }
+
+        private void textBox_DatabaseServer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(AnalyzerCollectData.PaginaPrincipala.filename);
+            XmlNode root = document.SelectSingleNode("Database");
+            XmlNode server = root.SelectSingleNode("server");
+            server.InnerText = textBox_DatabaseServer.Text;
+
+            document.Save(AnalyzerCollectData.PaginaPrincipala.filename);
+        }
+
+        private void textBox_DatabaseName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(AnalyzerCollectData.PaginaPrincipala.filename);
+            XmlNode root = document.SelectSingleNode("Database");
+            XmlNode dbname = root.SelectSingleNode("dbName");
+            dbname.InnerText = textBox_DatabaseName.Text;
+
+            document.Save(AnalyzerCollectData.PaginaPrincipala.filename);
+        }
+        private void textBox_DatabaseUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(AnalyzerCollectData.PaginaPrincipala.filename);
+            XmlNode root = document.SelectSingleNode("Database");
+            XmlNode username = root.SelectSingleNode("username");
+            username.InnerText = textBox_DatabaseUsername.Text;
+
+            document.Save(AnalyzerCollectData.PaginaPrincipala.filename);
+        }
+
+        private void textBox_DatabasePassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(AnalyzerCollectData.PaginaPrincipala.filename);
+            XmlNode root = document.SelectSingleNode("Database");
+            XmlNode password = root.SelectSingleNode("password");
+            password.InnerText = textBox_DatabasePassword.Password;
+
+            document.Save(AnalyzerCollectData.PaginaPrincipala.filename);
+        }
+
+        private void combobox_tip_DropDownClosed(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(AnalyzerCollectData.PaginaPrincipala.filename);
+            XmlNode root = document.SelectSingleNode("Database");
+            root.Attributes[0].Value = combobox_tip.Text;
+
+            document.Save(AnalyzerCollectData.PaginaPrincipala.filename);
+        }
+    }
+}
